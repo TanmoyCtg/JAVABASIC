32fbda754dd6ade81f9de9841e34baffe0bbafaf
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-12-30 15:07:54 +0600

    initial status

---
+++ src/SimpleArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SimpleArray {
+
+    public static void main(String[] args){
+        System.out.println("Enter three numbers: ");
+        double x, y, z;
+        Scanner in = new Scanner(System.in);
+        x = in.nextDouble();
+        y = in.nextDouble();
+        z = in.nextDouble();
+        double avg = Average(x,y,z);
+        System.out.println("The total average is: "+avg);
+    }
+
+    private static double Average(double num1,double num2,double num3){
+        double avg = num1+num2+num3/3.0;
+        return avg;
+    }
+}
