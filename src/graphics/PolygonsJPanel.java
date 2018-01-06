e85abd6de5a04ae92975e98937d82decaa491a44
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2018-01-06 19:28:22 +0600

    polygons

--- src/graphics/PolygonsJPanel.java
+++ src/graphics/PolygonsJPanel.java
@@ -1,4 +1,26 @@
 package graphics;
 
-public class PolygonsJPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class PolygonsJPanel extends JPanel{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        this.setBackground(Color.WHITE);
+        //draw polygon with polygons object
+        int xValues[] = {20,40,50,30,20,15};
+        int yValues[]={50,50,60,80,80,60};
+        Polygon polygon1 = new Polygon(xValues,yValues,6);
+        g.drawPolygon(polygon1);
+
+        //draw polylines with two arrays
+        int xValues1[] = {70,90,100,80,70,65,60};
+        int yValues1[] ={100,100,110,110,130,110,90};
+        g.drawPolyline(xValues1,yValues1,7);
+
+        // fill polygon with two arrays
+        int xValues2[] = {120,140,150,190};
+        int yValues2[] ={40,70,80,60};
+        g.fillPolygon(xValues2,yValues2,4);
+    }
 }
