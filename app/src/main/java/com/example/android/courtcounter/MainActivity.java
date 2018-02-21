3bc636729f06ad3954ad6979a630cba8ad78e9b7
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2018-02-21 23:34:33 +0600

    add button functionality

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int scoreTeamA = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+
+
     }
+    public void three_points(View view){
+        scoreTeamA +=3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void two_points(View view){
+        displayForTeamA(2);
+    }
+    public void free_throw(View view){
+        displayForTeamA(1);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
